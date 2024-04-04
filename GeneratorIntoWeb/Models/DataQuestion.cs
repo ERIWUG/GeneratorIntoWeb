38ab37ec4c8e2d120902f4df16f4848eb4433aec
ERIWUG <e.belyi2013@gmail.com>
2024-04-04 11:05:30 +0300

    Added form to GeneratorTicket View Deleted useless classes

--- GeneratorIntoWeb/Models/DataQuestion.cs
+++ GeneratorIntoWeb/Models/DataQuestion.cs
@@ -3,12 +3,12 @@ using System.Drawing;
 
 namespace GeneratorIntoWeb.Models
 {
-    public class DataQuestion(string text, int type, bool flag);
+    //public class DataQuestion(string text, int type, bool flag);
 
-    public class MyDataq(string n,string text,int type,bool flag) : DataQuestion(text,type,flag)
-    {
-        string N = n;
-    }
+    //public class MyDataq(string n,string text,int type,bool flag) : DataQuestion(text,type,flag)
+    //{
+    //    string N = n;
+    //}
 
    
 }
